57675d8621b8b886cd2272c80e6c9bc7027ca4ea
hugo costenoble <hugo.costenoble@bts-malraux.net>
2015-02-25 11:54:09 +0100

    démarrage du projet

--- mafenetreprincipale.h
+++ mafenetreprincipale.h
@@ -15,6 +15,15 @@ public:
     explicit MafenetrePrincipale(QWidget *parent = 0);
     ~MafenetrePrincipale();
 
+private slots:
+    void on_pushButton_2_clicked();
+
+    void on_horizontalSlider_2_valueChanged(int value);
+
+    void on_progressBar_valueChanged(int value);
+
+    void on_pushButton_2_destroyed();
+
 private:
     Ui::MafenetrePrincipale *ui;
 };
